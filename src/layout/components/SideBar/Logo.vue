1390f973ff1b949e0c588a5c1f885123a9a9626b
ssGISer <394087123@qq.com>
2019-10-21 19:25:53 +0800

    添加logo图标

--- src/layout/components/SideBar/Logo.vue
+++ src/layout/components/SideBar/Logo.vue
@@ -1,15 +1,34 @@
 <template>
     <div class="sidebar-logo-container" :class="{ 'collapse': collapse }">
         <transition name="sidebarLogoFade">
-            <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/"></router-link>
-            <router-link></router-link>
+            <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+                <img v-if="logo" :src="logo" class="sidebar-logo">
+                <h1 v-else class="sidebar-title">{{ title }}</h1>
+            </router-link>
+            <router-link v-else key="expand" class="sidebar-logo-link">
+                <img v-if="logo" :src="logo" class="sidebar-logo">
+                <h1 class="sidebar-title">{{ title }}</h1>
+            </router-link>
         </transition>
     </div>    
 </template>
 
 <script>
 export default {
-    
+    name: 'SidebarLogo',
+    props: {
+        collapse: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data () {
+        return {
+            title: 'Vue Cesium Demo',
+            logo: ''   
+        }
+    }
+
 }
 </script>
 
